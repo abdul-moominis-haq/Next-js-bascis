44874d77378c3d67ab9290d0f2644673efe812af
IS-HAQ ABDUL-MOOMIN <114371157+abdul-moominis-haq@users.noreply.github.com>
2024-05-16 23:22:46 +0000

    Update page.tsx
    App index page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProductCard from './components/ProductCard'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-center bg-green-950 text-gray-100 text-3xl '>UMaT VIRTUAL INTERNSHIP PLATFORM</h1>
+      <h1 className='text-center bg-green-950 text-gray-100 text-3xl '>NextJS Demo</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
       
